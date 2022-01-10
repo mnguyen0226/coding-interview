f936ee1a575f6a36b562a61d0944f53ecb99f8d5
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-09 20:34:07 -0500

    minor change

--- src/packages/cpp_data_structures/containers/unordered_map_stl.cpp
+++ src/packages/cpp_data_structures/containers/unordered_map_stl.cpp
@@ -1,1 +1,71 @@
 /*Illustration of unordered_map stl*/
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+/**
+ * @brief Runs capacity functions for unordered_map
+ * 
+ */
+void run_capacity()
+{
+    cout << "Test empty(): ";
+
+}
+
+/**
+ * @brief Runs iterators functions for unordered_map
+ * 
+ */
+void run_iterators()
+{
+}
+
+/**
+ * @brief Runs elements access functions for unordered_map
+ * 
+ */
+void run_element_access()
+{
+}
+
+/**
+ * @brief Runs elements lookup functions for unordered_map
+ * 
+ */
+void run_element_lookup()
+{
+}
+
+/**
+ * @brief Runs modifiers functions for unordered_map
+ * 
+ */
+void run_modifiers()
+{
+}
+
+/**
+ * @brief Runs buckets functions for unordered_map
+ * 
+ */
+void run_buckets()
+{
+}
+
+/**
+ * @brief Runs hash policy funcotion for unordered_map
+ * 
+ */
+void run_hash_policy()
+{
+}
+
+/**/
+int main()
+{
+
+    return 0;
+}
