da620ccbead19856b8c6488665b67767e629f7a8
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-10 11:55:53 -0500

    complete unordered_map

--- src/packages/cpp_data_structures/containers/unordered_map_stl.cpp
+++ src/packages/cpp_data_structures/containers/unordered_map_stl.cpp
@@ -113,12 +113,72 @@ void run_element_lookup_count()
 void run_modifiers()
 {
     unordered_map<int, string> mymap = {{1, "minh"}, {5, "123minh"}, {2, "nguyen"}, {3, "tran"}, {4, "binh"}};
+    cout << "Test emplace(): " << endl;
     mymap.emplace(6, "bruh");
 
     for (int i = 0; i < mymap.bucket_count(); i++)
     {
         cout << "The bucket #: " << i << " stores: ";
         for (auto iter = mymap.cbegin(i); iter != mymap.cend(i); iter++)
+        {
+            cout << "( " << iter->first << ", " << iter->second << "); ";
+        }
+        cout << endl;
+    }
+
+    // can insert a multimap to another multimap
+    cout << "Test insert(): " << endl;
+    mymap.insert({{7, "min"}, {8, "456"}});
+
+    for (int i = 0; i < mymap.bucket_count(); i++)
+    {
+        cout << "The bucket #: " << i << " stores: ";
+        for (auto iter = mymap.cbegin(i); iter != mymap.cend(i); iter++)
+        {
+            cout << "( " << iter->first << ", " << iter->second << "); ";
+        }
+        cout << endl;
+    }
+
+    cout << "Test erase(): " << endl;
+    mymap.erase(1);
+    for (int i = 0; i < mymap.bucket_count(); i++)
+    {
+        cout << "The bucket #: " << i << " stores: ";
+        for (auto iter = mymap.cbegin(i); iter != mymap.cend(i); iter++)
+        {
+            cout << "( " << iter->first << ", " << iter->second << "); ";
+        }
+        cout << endl;
+    }
+
+    cout << "Test clear(): " << endl;
+    mymap.clear();
+    if (mymap.empty())
+        cout << "mymap has been erase." << endl;
+    else
+        cout << "mymap has not been erase." << endl;
+
+    cout << "Test swap(): " << endl;
+    unordered_map<int, int> map1 = {{1, 123}, {2, 234}};
+    unordered_map<int, int> map2 = {{3, 345}, {4, 456}};
+    map1.swap(map2);
+
+    for (int i = 0; i < map1.bucket_count(); i++)
+    {
+        cout << "The bucket #: " << i << " stores: ";
+        for (auto iter = map1.cbegin(i); iter != map1.cend(i); iter++)
+        {
+            cout << "( " << iter->first << ", " << iter->second << "); ";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < map2.bucket_count(); i++)
+    {
+        cout << "The bucket #: " << i << " stores: ";
+        for (auto iter = map2.cbegin(i); iter != map2.cend(i); iter++)
         {
             cout << "( " << iter->first << ", " << iter->second << "); ";
         }
@@ -132,6 +192,18 @@ void run_modifiers()
  */
 void run_buckets()
 {
+    unordered_map<int, string> mymap = {{1, "minh"}, {5, "123minh"}, {2, "nguyen"}, {3, "tran"}, {4, "binh"}};
+    cout << "Bucket counts:  " << mymap.bucket_count() << endl
+         << endl;
+    for (int i = 0; i < mymap.bucket_count(); i++)
+    {
+        cout << "Bucket #" << i << " has size: " << mymap.bucket_size(i) << endl;
+    }
+    cout << endl;
+
+    cout << "Max bucket count: " << mymap.max_bucket_count() << endl;
+
+    cout << "Element key 1 is located at bucket: " << mymap.bucket(1) << endl;
 }
 
 /**
@@ -140,6 +212,39 @@ void run_buckets()
  */
 void run_hash_policy()
 {
+    cout << "Test loading factor: " << endl;
+    unordered_multimap<int, string> mymap;
+    cout << "Loading Factor: " << mymap.load_factor() << endl;
+    cout << "Max Loading Factor: " << mymap.max_load_factor() << endl;
+
+    cout << "Test rehash(): " << endl;
+    cout << "Current bucket_count: " << mymap.bucket_count() << endl;
+    mymap.rehash(11);
+    mymap.insert({1, "minh"});
+    mymap.insert({2, "nguyen"});
+    mymap.insert({100, "tran"});
+    cout << "After rehash() current bucket bucket_count: " << mymap.bucket_count() << endl;
+
+    // reserve() sets the number of buckets in the container (bucket_count) to the most approprate to contain at least n elements.
+    mymap.reserve(6);
+
+    cout << "mymap contains: ";
+    for (auto iter = mymap.begin(); iter != mymap.end(); iter++)
+    {
+        cout << "( " << iter->first << ", " << iter->second << "); ";
+    }
+    cout << endl;
+    cout << "Number of buckets: " << mymap.bucket_count() << endl;
+
+    for (int i = 0; i < mymap.bucket_count(); i++)
+    {
+        cout << "Bucket # " << i << " has a size: " << mymap.bucket_size(i) << " contains: ";
+        for (auto local_iter = mymap.begin(i); local_iter != mymap.end(i); local_iter++)
+        {
+            cout << "( " << local_iter->first << ", " << local_iter->second << "); ";
+        }
+        cout << endl;
+    }
 }
 
 /**/
@@ -150,6 +255,8 @@ int main()
     // run_element_access();
     // run_element_lookup_find();
     // run_element_lookup_count();
-    run_modifiers();
+    // run_modifiers();
+    // run_buckets0();
+    run_hash_policy();
     return 0;
 }
