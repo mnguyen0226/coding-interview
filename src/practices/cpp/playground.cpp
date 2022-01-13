c4cf296382156e8b0dc421151faeed1b9e848816
mnguyen0226 <mnguyen0226@vt.edu>
2022-01-12 23:56:44 -0500

    minor change

---
+++ src/practices/cpp/playground.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    array<int, 1001> arr = {};
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
